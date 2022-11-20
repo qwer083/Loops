b0f0831a9135c858c2f258da4fd912f143999e45
qwer083 <Qwertyiop890>
2022-11-20 18:54:55 +0300

    final

--- src/test/java/FizzBuzz.java
+++ src/test/java/FizzBuzz.java
@@ -1,15 +1,19 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else if
-            (i % 3 == 0 || i % 5 == 0) ;
+
+
+            for (int i = 0; i <=100; i++)
             {
-                System.out.println("FizzBuzz");
+                if (i % 3 == 0) {
+                    System.out.println("fizz");
+                }
+                if (i % 5 == 0) {
+                    System.out.println("buzz");
+                }
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                }
             }
         }
     }
-}
+
